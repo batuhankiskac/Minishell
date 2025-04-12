2c0064703205d15fecde4eaf15902dd73d576b8c
batuhankiskac <kiskacbatuhan@gmail.com>
2025-04-12 21:53:13 +0300

    Update exec_external.c and main.c: modify file headers and change external command test from "ls -l" to "date"

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 16:26:43 by bkiskac           #+#    #+#             */
-/*   Updated: 2025/04/12 20:08:57 by bkiskac          ###   ########.fr       */
+/*   Updated: 2025/04/12 21:48:34 by bkiskac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,18 +28,24 @@ int	main(int argc, char **argv, char **envp)
 		return (1);
 	}
 
-	/* External komut testi: "ls -l" */
-	cmd.cmd = ft_strdup("ls");
-	cmd.args = (char *[]){"ls", "-l", NULL};
-	cmd.argc = 2;
+	/* External komut testi: "date" */
+	cmd.cmd = ft_strdup("date");
+	if (!cmd.cmd)
+	{
+		ft_putendl_fd("Failed to duplicate command", 2);
+		free_env(shell.env);
+		return (1);
+	}
+	cmd.args = (char *[]){"date", NULL};
+	cmd.argc = 1;
 	shell.command = &cmd;
+
 	exit_status = exec_external(&shell);
 	ft_putstr_fd("External command exit status: ", 1);
 	ft_putnbr_fd(exit_status, 1);
 	ft_putchar_fd('\n', 1);
-	free(cmd.cmd);
 
-	/* Tüm environment kaynaklarını temizle */
+	free(cmd.cmd);
 	free_env(shell.env);
 	return (0);
 }
