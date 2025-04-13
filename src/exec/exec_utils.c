e985b5eea82e34ea7a94a407a4e346382a3a3bcb
batuhankiskac <kiskacbatuhan@gmail.com>
2025-04-13 15:42:41 +0300

    Update exec_utils.c: reformat function documentation for get_env, find_path, open_file, and dup_fd for improved readability and consistency.

--- src/exec/exec_utils.c
+++ src/exec/exec_utils.c
@@ -6,26 +6,26 @@
 /*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 16:53:36 by bkiskac           #+#    #+#             */
-/*   Updated: 2025/04/13 15:37:58 by bkiskac          ###   ########.fr       */
+/*   Updated: 2025/04/13 15:39:35 by bkiskac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
 /*
- * get_env - Retrieves the value of an environment variable from the envp array.
- *
- * This function searches for an environment variable in the envp array
- * and returns its value if found. The variable name is expected to be
- * in the format "NAME=VALUE".
- *
- * Parameters:
- *   char *name - The name of the environment variable to search for.
- *   char **envp - The array of environment variables.
- *
- * Returns:
- *   A pointer to the value of the environment variable, or NULL if not found.
- */
+* get_env - Retrieves the value of an environment variable from the envp array.
+*
+* This function searches for an environment variable in the envp array
+* and returns its value if found. The variable name is expected to be
+* in the format "NAME=VALUE".
+*
+* Parameters:
+*   char *name - The name of the environment variable to search for.
+*   char **envp - The array of environment variables.
+*
+* Returns:
+*   A pointer to the value of the environment variable, or NULL if not found.
+*/
 char	*get_env(char *name, char *envp[])
 {
 	int	i;
@@ -43,22 +43,22 @@ char	*get_env(char *name, char *envp[])
 }
 
 /*
- * find_path - Searches for the full path of a command
- * in the PATH environment variable.
- *
- * This function splits the PATH environment variable into its components
- * and checks each component to see if the command exists and is executable.
- * If found, it returns the full path to the command; otherwise, it returns
- * the original command string.
- *
- * Parameters:
- *   char *cmd - The command to search for.
- *   char **envp - The array of environment variables.
- *
- * Returns:
- *   A pointer to the full path of the command if found, or the original
- *   command string if not found.
- */
+* find_path - Searches for the full path of a command
+* in the PATH environment variable.
+*
+* This function splits the PATH environment variable into its components
+* and checks each component to see if the command exists and is executable.
+* If found, it returns the full path to the command; otherwise, it returns
+* the original command string.
+*
+* Parameters:
+*   char *cmd - The command to search for.
+*   char **envp - The array of environment variables.
+*
+* Returns:
+*   A pointer to the full path of the command if found, or the original
+*   command string if not found.
+*/
 char	*find_path(char *cmd, char *envp[])
 {
 	int		i;
@@ -89,21 +89,21 @@ char	*find_path(char *cmd, char *envp[])
 }
 
 /*
- * open_file - Opens a file with the specified flags and mode.
- *
- * This function attempts to open a file with the given flags and mode.
- * If the file cannot be opened, it prints an error message to stderr
- * and returns ERROR.
- *
- * Parameters:
- *   char *filename - The name of the file to open.
- *   int flags - The flags to use when opening the file.
- *   int mode - The mode to use when creating the file (if applicable).
- *   char *type - A string indicating the type of operation (for error messages).
- *
- * Returns:
- *   The file descriptor if successful, or ERROR if an error occurs.
- */
+* open_file - Opens a file with the specified flags and mode.
+*
+* This function attempts to open a file with the given flags and mode.
+* If the file cannot be opened, it prints an error message to stderr
+* and returns ERROR.
+*
+* Parameters:
+*   char *filename - The name of the file to open.
+*   int flags - The flags to use when opening the file.
+*   int mode - The mode to use when creating the file (if applicable).
+*   char *type - A string indicating the type of operation (for error messages).
+*
+* Returns:
+*   The file descriptor if successful, or ERROR if an error occurs.
+*/
 int	open_file(char *filename, int flags, int mode, char *type)
 {
 	int	fd;
@@ -121,20 +121,20 @@ int	open_file(char *filename, int flags, int mode, char *type)
 }
 
 /*
- * dup_fd - Duplicates a file descriptor to a new file descriptor.
- *
- * This function duplicates the old file descriptor to the new file descriptor
- * using dup2. If an error occurs, it prints an error message to stderr,
- * closes the old file descriptor, and returns ERROR.
- *
- * Parameters:
- *   int old_fd - The old file descriptor to duplicate.
- *   int new_fd - The new file descriptor to duplicate to.
- *   char *type - A string indicating the type of operation (for error messages).
- *
- * Returns:
- *   0 if successful, or ERROR if an error occurs.
- */
+* dup_fd - Duplicates a file descriptor to a new file descriptor.
+*
+* This function duplicates the old file descriptor to the new file descriptor
+* using dup2. If an error occurs, it prints an error message to stderr,
+* closes the old file descriptor, and returns ERROR.
+*
+* Parameters:
+*   int old_fd - The old file descriptor to duplicate.
+*   int new_fd - The new file descriptor to duplicate to.
+*   char *type - A string indicating the type of operation (for error messages).
+*
+* Returns:
+*   0 if successful, or ERROR if an error occurs.
+*/
 int	dup_fd(int old_fd, int new_fd, char *type)
 {
 	if (dup2(old_fd, new_fd) == -1)
