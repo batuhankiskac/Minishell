4ae13cd41729474e44742b5fb014fe29696c1560
batuhankiskac <kiskacbatuhan@gmail.com>
2025-04-19 22:23:40 +0300

    Update exec_utils_2.c and execute.c: add function documentation for close_pipe_fd, run_command, setup_pipe_for_cmd, update_parent_pipe, execute_pipe_child, handle_pipe_iteration, and execute_pipe

--- src/exec/exec_utils_2.c
+++ src/exec/exec_utils_2.c
@@ -6,12 +6,21 @@
 /*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 20:12:40 by bkiskac           #+#    #+#             */
-/*   Updated: 2025/04/19 20:34:59 by bkiskac          ###   ########.fr       */
+/*   Updated: 2025/04/19 22:22:31 by bkiskac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+/*
+* close_pipe_fd - close file descriptors for pipes and previous fd
+*
+* Closes the previous file descriptor and both ends of the pipe.
+*
+* Parameters:
+*   int prev_fd       - previous file descriptor to close
+*   int pipe_fd[2]    - array containing pipe file descriptors
+*/
 void	close_pipe_fd(int prev_fd, int pipe_fd[2])
 {
 	if (prev_fd != -1)
@@ -22,6 +31,15 @@ void	close_pipe_fd(int prev_fd, int pipe_fd[2])
 		close(pipe_fd[1]);
 }
 
+/*
+* run_command - execute the command in the shell context
+*
+* Checks if the command is a builtin or an external command and executes it.
+* If the command is a pipeline, it calls execute_pipe to handle the pipeline.
+*
+* Parameters:
+*   t_shell *shell    - shell context containing the command to execute
+*/
 void	run_command(t_shell *shell)
 {
 	if (!shell->command)
--- src/exec/execute.c
+++ src/exec/execute.c
@@ -6,12 +6,24 @@
 /*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 19:54:31 by bkiskac           #+#    #+#             */
-/*   Updated: 2025/04/19 20:33:07 by bkiskac          ###   ########.fr       */
+/*   Updated: 2025/04/19 22:21:20 by bkiskac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+/*
+* setup_pipe_for_cmd - prepare a pipe for the given command
+*
+* Checks if cmd->next exists; if so, opens a new pipe and fills pipe_fd.
+*
+* Parameters:
+*   t_command *cmd    - current command node
+*   int pipe_fd[2]    - array to receive pipe file descriptors
+*
+* Returns:
+*   0 on success, ERROR on failure
+*/
 static int	setup_pipe_for_cmd(t_command *cmd, int pipe_fd[2])
 {
 	if (cmd->next)
@@ -27,6 +39,16 @@ static int	setup_pipe_for_cmd(t_command *cmd, int pipe_fd[2])
 	return (0);
 }
 
+/*
+* update_parent_pipe - update and close parent side of previous pipe
+*
+* Closes old prev_fd, assigns new read end for next command, closes write end.
+*
+* Parameters:
+*   int *prev_fd      - pointer to previous read fd
+*   int pipe_fd[2]    - current pipe fds
+*   t_command *cmd    - current command node
+*/
 static void	update_parent_pipe(int *prev_fd, int pipe_fd[2], t_command *cmd)
 {
 	if (*prev_fd != -1)
@@ -40,6 +62,17 @@ static void	update_parent_pipe(int *prev_fd, int pipe_fd[2], t_command *cmd)
 		*prev_fd = -1;
 }
 
+/*
+* execute_pipe_child - child process routine for piped commands
+*
+* Duplicates prev_fd to stdin, pipe_write_fd to stdout, sets up redirections,
+* runs builtin or external command, then exits with its return code.
+*
+* Parameters:
+*   t_shell *shell        - shell context with command info
+*   int prev_fd           - fd for reading from previous pipe
+*   int pipe_write_fd     - fd for writing to next pipe
+*/
 static void	execute_pipe_child(t_shell *shell, int prev_fd, int pipe_write_fd)
 {
 	int	ret;
@@ -67,6 +100,19 @@ static void	execute_pipe_child(t_shell *shell, int prev_fd, int pipe_write_fd)
 	exit(ret);
 }
 
+/*
+* handle_pipe_iteration - fork and execute one stage of a pipeline
+*
+* Sets up pipe, forks, in child calls execute_pipe_child, parent updates prev_fd.
+*
+* Parameters:
+*   t_shell *shell        - shell context with command list
+*   t_command *cmd        - current command node
+*   int *prev_fd          - pointer to previous pipe read end
+*
+* Returns:
+*   0 on success, ERROR on failure
+*/
 static int	handle_pipe_iteration(t_shell *shell, t_command *cmd, int *prev_fd)
 {
 	int		pipe_fd[2];
@@ -92,6 +138,18 @@ static int	handle_pipe_iteration(t_shell *shell, t_command *cmd, int *prev_fd)
 	return (0);
 }
 
+/*
+* execute_pipe - execute a sequence of piped commands
+*
+* Iterates through commands linked list, handling each pipe iteration,
+* then waits for all children and returns the exit status of the last.
+*
+* Parameters:
+*   t_shell *shell    - shell context containing command pipeline
+*
+* Returns:
+*   exit status of last command or ERROR
+*/
 int	execute_pipe(t_shell *shell)
 {
 	t_command	*cmd;
