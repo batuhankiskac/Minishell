5d6b8fda507ba9bfcf72ead1d6ff265732a9b346
batuhankiskac <kiskacbatuhan@gmail.com>
2025-04-22 21:31:28 +0300

    Remove unused printf functions and update Makefile and header file timestamps

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 12:03:39 by bkiskac           #+#    #+#             */
-/*   Updated: 2025/04/13 15:10:24 by bkiskac          ###   ########.fr       */
+/*   Updated: 2025/04/22 21:31:09 by bkiskac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,6 @@
 
 # include <stdlib.h>
 # include <unistd.h>
-# include <stdarg.h>
-
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 32
-# endif
 
 typedef struct s_list
 {
@@ -39,7 +34,6 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_lstsize(t_list *lst);
-int		ft_printf(const char *format, ...);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
@@ -51,7 +45,6 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_itoa(int n);
 char	*ft_strmapi(const char *s, char (*f)(unsigned int, char));
 char	**ft_split(char const *s, char c);
-char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
@@ -71,12 +64,6 @@ void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
-void	ft_putptr(void *ptr, unsigned int *count);
-void	ft_puthexp(unsigned long n, unsigned int *count);
-void	ft_puthex(unsigned int hex, char format_specifier, unsigned int *count);
-void	ft_putunbr(unsigned int n, unsigned int *count);
-void	ft_putnbr(int n, unsigned int *count);
-void	ft_putstr(const char *s, unsigned int *count);
 t_list	*ft_lstnew(void *content);
 t_list	*ft_lstlast(t_list *lst);
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
